b50cc0c0cc56f8e2ac9dcddb71ade6b2b145c014
Grant Ho <55564015+gho19@users.noreply.github.com>
2020-05-22 12:27:17 -0600

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public Main() {
+
+        JFrame frame = new JFrame();
+        Screen screen = new Screen();
+
+        frame.add(screen);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Snake Game");
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
